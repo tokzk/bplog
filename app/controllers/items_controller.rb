d62037c065f949ac283d6da34589eedf6cbace0a
tokzk <t@okzk.org>
2015-06-10 17:59:48 +0900

    Fix amazon search

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,6 @@
-require 'amazon/ecs'
-
 class ItemsController < ApplicationController
   def index
-    search_term = params[:search] || 'harry potter'
-    item_page = params[:page] || '1'
-    
-    items, total_count = Item.search(search_term, item_page)
+    items, total_count = Item.search(params[:search], params[:page])
     @items = Kaminari.paginate_array(items, total_count: total_count).page(params[:page]).per(10)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,42 +1,40 @@
 class Item
   include ActiveAttr::Model
 
+  attribute :asin
   attribute :title
   attribute :author
   attribute :manufacturer
   attribute :product_group
-  attribute :asin
+  attribute :url
   attribute :image_url
 
-  def self.search(search_term, item_page)
-    @res  = Amazon::Ecs.item_search(search_term, { search_index: 'Books',
-                                                   item_page: item_page,
-                                                   sort: 'salesrank',
-                                                   country: 'jp'})
-    begin
-      @imgs = Amazon::Ecs.item_search(search_term, { response_group: 'Images',
-                                                     search_index: 'Books',
-                                                     item_page: item_page,
-                                                     sort: 'salesrank',
-                                                     country: 'jp' })
-    rescue => e
-      logger.debug e.message
-      logger.debug @res
-    end
+  def self.search(search_term, page = 1)
+    return [[], 0] if search_term.blank?
+    res  = Amazon::Ecs.item_search(search_term, { response_group: 'Medium',
+                                                  search_index: 'Books',
+                                                  item_page: page,
+                                                  sort: 'salesrank',
+                                                  country: 'jp'})
 
-    items = @res.items.map.with_index do |item, n|
+    items = res.items.map.with_index do |item, n|
       item_attributes = item.get_element('ItemAttributes')
-      item = Item.new
-      item.title  = item_attributes.get('Title')
-      item.author = item_attributes.get('Author')
-      item.manufacturer = item_attributes.get('Manufacturer')
-      item.product_group = item_attributes.get('ProductGroup')
-      item.asin = @imgs.items[n].get('ASIN')
-      item.image_url = @imgs.items[n].get('SmallImage/URL')
-      item
+      Item.new(
+        asin:          item.get('ASIN'),
+        title:         item_attributes.get('Title'),
+        author:        item_attributes.get('Author'),
+        manufacturer:  item_attributes.get('Manufacturer'),
+        product_group: item_attributes.get('ProductGroup'),
+        url:           item.get('DetailPageURL'),
+        image_url:     item.get('LargeImage/URL')
+      )
     end
-    total_count = @res.total_results > 100 ? 100 : @res.total_results
-    [items, total_count]
+    [items, total_count(res)]
   end
-  
+
+  private
+
+  def self.total_count(res)
+    res.total_results > 100 ? 100 : res.total_results
+  end
 end
